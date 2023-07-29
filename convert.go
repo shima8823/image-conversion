0dd55b56a9832a04c4076400cdddd761059f056e
shima8823 <taiyaki1275@gmail.com>
2023-07-29 17:07:19 +0900

    refactor: close #2 ユーザー定義型でリファクタリングする

--- convert.go
+++ convert.go
@@ -3,11 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
 	"image/jpeg"
 	"image/png"
 	"os"
 	"path/filepath"
 )
+
+type ImageFile struct {
+	Path string
+	Img  image.Image
+}
 
 func main() {
 	flag.Parse()
@@ -26,25 +32,14 @@ func walkJpg(root string) error {
 	err := filepath.Walk(root,
 		func(path string, info os.FileInfo, err error) error {
 			if filepath.Ext(path) == ".jpg" {
-				jpgFile, err := os.Open(path)
-				if handleError(path, err) != nil {
+				imageFile, err := NewImageFile(&ImageFile{Path: path})
+				if err != nil {
+					handleError(path)
 					return nil
 				}
-				defer jpgFile.Close()
-
-				img, err := jpeg.Decode(jpgFile)
-				if handleError(path, err) != nil {
-					return nil
-				}
-
-				pngFile, err := os.Create(getFileNameWithoutExt(path) + ".png")
-				if handleError(path, err) != nil {
-					return nil
-				}
-				defer pngFile.Close()
-
-				err = png.Encode(pngFile, img)
-				if handleError(path, err) != nil {
+				err = ConvertToPng(imageFile)
+				if err != nil {
+					handleError(path)
 					return nil
 				}
 			}
@@ -60,10 +55,35 @@ func getFileNameWithoutExt(path string) string {
 	return path[:len(path)-len(filepath.Ext(path))]
 }
 
-func handleError(path string, err error) error {
+func NewImageFile(imageFile *ImageFile) (*ImageFile, error) {
+	jpgFile, err := os.Open(imageFile.Path)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "error:", path, "is not a valid file")
+		return nil, err
+	}
+	defer jpgFile.Close()
+
+	img, err := jpeg.Decode(jpgFile)
+	if err != nil {
+		return nil, err
+	}
+	return &ImageFile{Path: imageFile.Path, Img: img}, nil
+}
+
+func ConvertToPng(imageFile *ImageFile) error {
+	pngFile, err := os.Create(getFileNameWithoutExt(imageFile.Path) + ".png")
+	if err != nil {
+		return err
+	}
+	defer pngFile.Close()
+	defer fmt.Println("converted:", pngFile.Name())
+
+	err = png.Encode(pngFile, imageFile.Img)
+	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func handleError(path string) {
+	fmt.Fprintln(os.Stderr, "error:", path, "is not a valid file")
 }
